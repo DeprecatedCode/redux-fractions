1ca7f23753cb66a66c9e5e4c43f860eb1d795494
Nate Ferrero <nateferrero@gmail.com>
2018-01-22 12:28:20 -0500

    Reducers are now workin

--- index.ts
+++ index.ts
@@ -1,20 +1,78 @@
 import { connect as reduxConnect } from 'react-redux'
+import { createStore as reduxCreateStore } from 'redux'
 import { Component } from 'react'
 
+interface IFluxStandardAction {
+  type: string,
+  error?: boolean,
+  payload?: any
+  meta?: object
+}
+
+export interface IActionReducers<TComponentState> {
+  [key: string]: ((state: TComponentState, payload?: any, error?: boolean) => TComponentState)
+}
+
+const reducers: IActionReducers<{}> = {}
+const initialRootState: { [key: string]: any } = {}
+
 // This is complicated - we are ensuring type information can be passed through to redux
-// Details: { new(): TComponentClass } is a constructor type signature
+// Details: { new(props: any): TComponentClass } is a constructor type signature
 export function connect<
-  TComponent extends Component<TComponentState & TComponentActions>,
+  TComponentClass extends Component<TComponentState & TComponentActions>,
   TComponentState,
   TComponentActions
->(ComponentClass: { new(props: any): TComponent }, intialState: TComponentState, actions: object) {
+>(ComponentClass: { new(props: any): TComponentClass }, initialState: TComponentState, actions: IActionReducers<TComponentState>) {
+  initialRootState[ComponentClass.name] = initialState
 
   const mapStateToProps = (state: any): TComponentState =>
-    ComponentClass.name in state ? state[ComponentClass.name] : intialState
+    ComponentClass.name in state ? state[ComponentClass.name] : initialState
 
   const mapDispatchToProps = (dispatch: any) => {
-    return {}
+    const actionCreators: {[key: string]: any} = {}
+
+    Object.keys(actions).forEach(key => {
+      const actionType = `${ComponentClass.name}:${key}`
+
+      actionCreators[key] = (payload: any, error: boolean = false) => {
+        const action: IFluxStandardAction = {
+          type: actionType
+        }
+        if (payload) {
+          action.payload = payload
+        }
+        if (error) {
+          action.error = error
+        }
+        dispatch(action)
+      }
+
+      reducers[actionType] = actions[key]
+    })
+
+    return actionCreators as TComponentActions
   }
 
   return reduxConnect(mapStateToProps, mapDispatchToProps)(ComponentClass)
+}
+
+export function createStore() {
+  return reduxCreateStore((rootState: { [key: string]: object }={}, action: IFluxStandardAction) => {
+    console.log(action)
+    if (action.type.indexOf(':') === - 1) {
+      return rootState
+    }
+
+    const [ namespace, name ] = action.type.split(':')
+
+    const state = namespace in rootState ? rootState[namespace] : initialRootState[namespace]
+
+    return {
+      ...rootState,
+      [namespace]: {
+        ...state,
+        ...reducers[action.type](state, action.payload, action.error)
+      }
+    }
+  })
 }
