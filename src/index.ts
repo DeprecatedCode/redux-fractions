51c6767218f7ed82e98972c6c9a87d856989d178
Nate Ferrero <nateferrero@gmail.com>
2018-01-22 11:25:44 -0500

    Initial progress

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { connect as reduxConnect } from 'react-redux'
+import { Component } from 'react'
+
+// This is complicated - we are ensuring type information can be passed through to redux
+// Details: { new(): TComponentClass } is a constructor type signature
+export function connect<
+  TComponentClass extends Component<TComponentState & TComponentActions>,
+  TComponentState,
+  TComponentActions
+>(ComponentClass: { new(): TComponentClass }, intialState: TComponentState, actions: object) {
+
+  const mapStateToProps = (state: any): TComponentState =>
+    ComponentClass.name in state ? state[ComponentClass.name] : intialState
+
+  const mapDispatchToProps = dispatch => {
+    return {}
+  }
+
+  return reduxConnect(mapStateToProps, mapDispatchToProps)(ComponentClass)
+}
